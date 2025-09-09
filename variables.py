4fc89129094378f72e607086e869ec36e78cf12d
Ritterl27 <ritterl27@chcstudent.com>
2025-09-09 13:56:44 -0400

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,14 @@
+x = 5
+print(x)
+y = 10 
+print (x + y)
+print (x * y)
+print (x / y)
+print (x-y)
+print (x ** 2)
+sum = x + y 
+print (sum) 
+sum = sum + 47 
+print (sum)
+name = connor murray 
+print (f"my name is {name}")
