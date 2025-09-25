3171aced8945d097b5489c0e582d97bc5d6a24e3
Ritterl27 <ritterl27@chcstudent.com>
2025-09-25 13:59:01 -0400

    pp

---
+++ list.py
@@ -0,0 +1,19 @@
+mylist = [5,10,15,20]
+print(mylist)
+print(mylist[0])
+sum = mylist[1] + mylist [2]
+print(sum)
+i = 0
+while i <=3:
+    print (mylist[i])
+    i = i + 1
+    for num in mylist:
+        num = num + 5
+
+print(mylist)
+
+i = 0 
+while i <=3:
+    mylist[i] = mylist[i] + 5
+
+print (mylist)
