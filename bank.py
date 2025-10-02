83ef716ab4b61c2a8b142aacc038c5e43c4388af
MurrayC27 <MurrayC27@chcstudent.com>
2025-10-02 14:44:49 -0400

    commit

---
+++ bank.py
@@ -0,0 +1,10 @@
+print ("welcome to FFCU How can I help you ")
+print ("deposit or withdraw ")
+print ("Transpher Money")
+print ("listy acounts and balance")
+print ("add or remove acount")
+option = input("")
+if(option == "deposit or withdraw"):
+    print()
+name = ["Connor , Jack , john , josh"]
+balance = [100 , 39 , 47 , 90]
