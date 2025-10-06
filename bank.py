46d5cef43c21e191144127df2acb795a37c33b6e
MurrayC27 <MurrayC27@chcstudent.com>
2025-10-06 13:53:57 -0400

    bank

--- bank.py
+++ bank.py
@@ -1,10 +1,16 @@
-print ("welcome to FFCU How can I help you ")
-print ("deposit or withdraw ")
-print ("Transpher Money")
-print ("listy acounts and balance")
-print ("add or remove acount")
-option = input("")
-if(option == "deposit or withdraw"):
-    print()
 name = ["Connor , Jack , john , josh"]
-balance = [100 , 39 , 47 , 90]
+balance = [39 , 47 , 90 , 100]
+check = False
+while check == False:
+    for i in range(len(name)):
+        print(f"Name:{name[1]} balance:{balance[1]}")
+    print ("welcome to FFCU How can I help you ")
+    print ("deposit or withdraw ")
+    print ("Transpher Money")
+    print ("listy acounts and balance")
+    print ("add or remove acount")
+option = input("")
+if(option == "deposit"):
+    print("how much do you want to deposit")
+if(option == "withdraw"):
+    print("how much do you want to withdraw")
